a529144a687d1a333f7336ee722d9b10b4462eb5
MuriloAdrianolilo <muriloadriano10@outlook.com>
2025-11-26 20:32:05 -0300

    Pokedex

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+
+import { Component, inject } from '@angular/core';
+import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/angular/standalone';
+import { DataService, Message } from '../services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  private data = inject(DataService);
+  constructor() {}
+
+  refresh(ev: any) {
+    setTimeout(() => {
+      (ev as RefresherCustomEvent).detail.complete();
+    }, 3000);
+  }
+
+  getMessages(): Message[] {
+    return this.data.getMessages();
+  }
+}
