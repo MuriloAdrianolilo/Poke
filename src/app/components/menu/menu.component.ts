03856c703a480e1173a2f6d4694d581f6f550e1a
MuriloAdrianolilo <muriloadriano10@outlook.com>
2025-11-26 22:08:25 -0300

    Menu Finalizado

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTab,
+  IonFooter,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { homeOutline, bugOutline, heartOutline, helpOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-menu',
+  standalone: true,
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  imports: [IonContent, IonHeader, IonIcon, IonTab, IonFooter, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar],
+
 })
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ homeOutline, bugOutline, heartOutline, helpOutline });
+ }
 
   ngOnInit() {}
 
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonFooter]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MenuComponent]
 })
 export class HomePage implements OnInit {
 
