5bc4153561d52084ca06f6dc9d2b475b3b1aa198
MuriloAdrianolilo <muriloadriano10@outlook.com>
2025-11-27 18:55:51 -0300

    codigo

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  IonContent,
-  IonHeader,
   IonIcon,
-  IonTab,
   IonFooter,
   IonTabBar,
   IonTabButton,
   IonTabs,
-  IonTitle,
-  IonToolbar,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { homeOutline, bugOutline, heartOutline, helpOutline } from 'ionicons/icons';
@@ -20,7 +15,7 @@ import { Router } from '@angular/router';
   standalone: true,
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonContent, IonHeader, IonIcon, IonTab, IonFooter, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar],
+  imports: [ IonIcon, IonFooter, IonTabBar, IonTabButton, IonTabs],
 
 })
 export class MenuComponent implements OnInit {
--- src/app/pages/pokedex/pokedex.page.ts
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -3,19 +3,31 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { IonAvatar, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
+import { PokedexService, Pokemon } from '../../services/pokedex/pokedex-service';
+import { firstValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.page.html',
   styleUrls: ['./pokedex.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MenuComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MenuComponent, IonAvatar, IonItem, IonLabel, IonList]
 })
 export class PokedexPage implements OnInit {
+  public pokedex: Pokemon[] = [];
 
-  constructor() { }
+  constructor(
+    private pokedexservice: PokedexService
+  ) {
 
-  ngOnInit() {
+  }
+
+  async ngOnInit() {
+    const pokemonObservable = this.pokedexservice.loadData();
+    this.pokedex = await firstValueFrom(pokemonObservable);
+    console.log (this.pokedex);
   }
 
 }
