400ba7f74cffe9408d2454a1bbb0f3d2842c6f09
MuriloAdrianolilo <muriloadriano10@outlook.com>
2025-11-27 21:40:34 -0300

    Colocamos cor nos tipos

--- src/app/pages/pokedex/pokedex.page.ts
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { MenuComponent } from '../../components/menu/menu.component';
-import { IonAvatar, IonItem, IonLabel, IonList, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { IonAvatar, IonItem, IonLabel, IonList, IonSelect, IonSelectOption, IonNote } from '@ionic/angular/standalone';
 import { PokedexService, Pokemon } from '../../services/pokedex/pokedex-service';
 import { firstValueFrom } from 'rxjs';
 
@@ -13,11 +13,12 @@ import { firstValueFrom } from 'rxjs';
   templateUrl: './pokedex.page.html',
   styleUrls: ['./pokedex.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MenuComponent, IonAvatar, IonItem, IonLabel, IonList, IonSelect, IonSelectOption]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MenuComponent, IonAvatar, IonItem, IonLabel, IonList, IonSelect, IonSelectOption, IonNote]
 })
 export class PokedexPage implements OnInit {
   public pokedex: Pokemon[] = [];
   public pokedexfull: Pokemon[] = [];
+  public colors: Record<string, string> = {};
 
   constructor(
     private pokedexservice: PokedexService
@@ -30,6 +31,7 @@ export class PokedexPage implements OnInit {
     this.pokedexfull = await firstValueFrom(pokemonObservable);
     const pokedextemp = this.pokedexfull;
     this.pokedex = pokedextemp.filter(pokemon => pokemon.gen === 1);
+    this.colors = this.pokedexservice.getColors();
   }
 
   formatarNumero(num: number): string {
@@ -48,6 +50,17 @@ export class PokedexPage implements OnInit {
       this.pokedex = pokedextemp.filter(pokemon => pokemon.gen === gen);
 
     }
+  }
+
+  getColor(type: string) {
+
+    console.log(type);
+    console.log(this.colors[type]);
+
+    const lowerCaseType = type.toLowerCase();
+    const color = this.colors[lowerCaseType] || '#cccccc';
+
+    return color;
   }
 
 }
